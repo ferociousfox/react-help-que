b8b1b5b5302f99ab56ffa876848b727fc14763f3
Chris Knight Johnson <cknightjohnson@gmail.com>
2018-02-11 16:56:52 -0800

    add linter rules

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ function Header(){
   return (
     <div>
       <h2>Header Content</h2>
-      <Link to="/">Home</Link> | <Link to="/newitem">Create Item</Link>
+      <Link to="/">List</Link> | <Link to="/newitem">Create Item</Link>
     </div>
   );
 }
